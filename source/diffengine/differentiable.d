5555b1d66cf574c6712da6cb20d22c2292212db4
outland.karasu <outland.karasu@gmail.com>
2021-03-10 09:00:06 +0900

    add types.

--- source/diffengine/differentiable.d
+++ source/diffengine/differentiable.d
@@ -2,3 +2,56 @@
 Differentiable type.
 */
 module diffengine.differentiable;
+
+@safe:
+
+/**
+Differentiable interface.
+
+Params:
+    R = result type.
+    A = arugment type.
+*/
+interface Differentiable(R, A)
+{
+    /**
+    Evaluate function.
+
+    Params:
+        argument = function argument.
+    Returns;
+        function result.
+    */
+    R opCall(return scope A argument) const nothrow pure return scope;
+
+    /**
+    Differentiate function.
+
+    Returns:
+        Differentiate result.
+    */
+    DiffResult!R differentiate(return scope DiffArgument!A argument) const nothrow pure return scope;
+}
+
+/**
+Differentiable function result and differentiated function.
+
+Params:
+    R = result type.
+    A = arugment type.
+*/
+struct DiffResult(R, A)
+{
+    R result;
+    Differentiable!(R, A) diff;
+}
+
+/**
+Differentiable function arugment.
+*/
+struct DiffAugument(A)
+{
+    bool target;
+    A value;
+}
+
