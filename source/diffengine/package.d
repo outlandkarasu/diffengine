7449271e7b57d47312fb124eee80a152283ef84c
outland.karasu <outland.karasu@gmail.com>
2021-03-13 22:11:46 +0900

    issue #5 update package.

--- source/diffengine/package.d
+++ source/diffengine/package.d
@@ -9,4 +9,6 @@ public import diffengine.parameter;
 public import diffengine.plus_minus;
 public import diffengine.mul;
 public import diffengine.div;
+public import diffengine.pow;
+public import diffengine.exp;
 
