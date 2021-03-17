26c3662d51c556a9263980df5963fc1840323089
outland.karasu <outland.karasu@gmail.com>
2021-03-17 12:34:55 +0900

    issue #13 add log function.

--- source/diffengine/log.d
+++ source/diffengine/log.d
@@ -3,8 +3,8 @@ Differentiable log function module.
 */
 module diffengine.log;
 
-import std.math : log;
-import std.traits : isFloatingPoint;
+import std.math : mathLog = log;
+import std.traits : isFloatingPoint, isNumeric;
 
 import diffengine.differentiable :
     Differentiable,
@@ -33,13 +33,13 @@ private final class Log(R) : Differentiable!R
 
     override R opCall() const nothrow pure return scope
     {
-        return log(x_());
+        return mathLog(x_());
     }
 
     DiffResult!R differentiate(scope const(DiffContext!R) context) const nothrow pure return scope
     {
         auto xResult = x_.differentiate(context);
-        R result = log(xResult.result);
+        R result = mathLog(xResult.result);
         auto dlog = div(context.one, xResult.result.constant);
         return DiffResult!R(result, mul(dlog, xResult.diff));
     }
@@ -64,16 +64,37 @@ nothrow pure unittest
 
     auto p = param(3.0);
     auto plog = p.log();
-    assert(plog().isClose(log(3.0)));
+    assert(plog().isClose(mathLog(3.0)));
 
     auto context = p.diffContext;
     auto plogd = plog.differentiate(context);
-    assert(plogd.result.isClose(log(3.0)));
+    assert(plogd.result.isClose(mathLog(3.0)));
     assert(plogd.diff().isClose(1.0/3.0));
 
     auto plog2x = log(p.square);
     auto plog2xd = plog2x.differentiate(context);
-    assert(plog2xd.result.isClose(log(9.0)));
+    assert(plog2xd.result.isClose(mathLog(9.0)));
     assert(plog2xd.diff().isClose(2.0/3.0));
+}
+
+/**
+log for numeric.
+
+Params:
+    x = value
+Returns:
+    log value.
+*/
+T log(T)(T x) @nogc nothrow pure
+    if (isNumeric!T)
+{
+    return mathLog(x);
+}
+
+///
+@nogc nothrow pure unittest
+{
+    import std.math : isClose;
+    assert(log(4.0).isClose(mathLog(4.0)));
 }
 
